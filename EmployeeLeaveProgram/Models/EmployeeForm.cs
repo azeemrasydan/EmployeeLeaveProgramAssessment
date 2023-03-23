f471e32c0a67b39f8ff63333cbac8966bf7f70a1
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-03-23 21:26:40 +0800

    Added model for Employee & EmployeeForm

---
+++ EmployeeLeaveProgram/Models/EmployeeForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeLeaveProgram.Models
+{
+    public class EmployeeLeaveForm
+    {
+        public string EmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set;}
+        public string Justification { get; set; }
+        public string ManagerEmployeeId { get; set; }
+
+    }
+}
