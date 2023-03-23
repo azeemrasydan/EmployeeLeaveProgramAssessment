3e4de6abb2f7bedbf49b9788dcb832f5a8b615b3
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-03-23 23:32:55 +0800

    :construction: Developing feature to submit form to DB as well as sending email after successfully submitted form to DB

--- EmployeeLeaveProgram/Controllers/HomeController.cs
+++ EmployeeLeaveProgram/Controllers/HomeController.cs
@@ -1,16 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EmployeeLeaveProgram.Data;
+using EmployeeLeaveProgram.Models;
+using System;
 using System.Web.Mvc;
 
 namespace EmployeeLeaveProgram.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationContext db = new ApplicationContext();
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            var employeeLeaveForm = new EmployeeLeaveForm()
+            {
+                EmployeeId = Request["employeeId"],
+                StartDate = Convert.ToDateTime(Request["startDate"]),
+                EndDate = Convert.ToDateTime(Request["endDate"]),
+                Justification = Request["justification"],
+                ManagerEmployeeId = Request["managerEmployeeId"]
+
+            };
+
+            CommitEmployeeFormToDb(employeeLeaveForm);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private int CommitEmployeeFormToDb(EmployeeLeaveForm employeeLeaveForm)
+        {
+            var applicant = db.Employees.Find(employeeLeaveForm.EmployeeId);
+            var manager = db.Employees.Find(employeeLeaveForm.ManagerEmployeeId);
+
+            var employeeLeaveApplication = new LeaveApplication() { 
+                Justification = employeeLeaveForm.Justification,
+                StartDate = employeeLeaveForm.StartDate,
+                EndDate = employeeLeaveForm.EndDate,
+                Applicant = applicant,
+                Manager = manager
+            };
+
+            db.LeaveApplications.Add(employeeLeaveApplication);
+
+            return db.SaveChanges(); ;
         }
 
         public ActionResult About()
