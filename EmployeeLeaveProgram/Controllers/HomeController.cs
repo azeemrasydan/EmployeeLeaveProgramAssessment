4e8028e482d7504e533c9efda95fb5bdf2466d92
azeemrasydan <azeem.rasydan@teras.com.my>
2023-03-24 12:36:09 +0800

    Successfully added email notifications for Approval and Rejection as well as Request For Approval of Leave Application

--- EmployeeLeaveProgram/Controllers/HomeController.cs
+++ EmployeeLeaveProgram/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeLeaveProgram.Data;
 using EmployeeLeaveProgram.Models;
 using System;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace EmployeeLeaveProgram.Controllers
@@ -27,17 +28,24 @@ namespace EmployeeLeaveProgram.Controllers
 
             };
 
-            CommitEmployeeFormToDb(employeeLeaveForm);
+            var applicant = db.Employees.Find(employeeLeaveForm.EmployeeId);
+
+            var manager = db.Employees.Find(employeeLeaveForm.ManagerEmployeeId);
+
+            var leaveApplication = CommitEmployeeFormToDb(applicant, manager, employeeLeaveForm);
+
+            SendNewLeaveApplicationEmail(
+                employeeLeaveApplication: leaveApplication
+                );
 
             return RedirectToAction("Index", "Home");
         }
 
-        private int CommitEmployeeFormToDb(EmployeeLeaveForm employeeLeaveForm)
+        private LeaveApplication CommitEmployeeFormToDb(Employee applicant, Employee manager, EmployeeLeaveForm employeeLeaveForm)
         {
-            var applicant = db.Employees.Find(employeeLeaveForm.EmployeeId);
-            var manager = db.Employees.Find(employeeLeaveForm.ManagerEmployeeId);
 
-            var employeeLeaveApplication = new LeaveApplication() { 
+            var employeeLeaveApplication = new LeaveApplication()
+            {
                 Justification = employeeLeaveForm.Justification,
                 StartDate = employeeLeaveForm.StartDate,
                 EndDate = employeeLeaveForm.EndDate,
@@ -48,7 +56,28 @@ namespace EmployeeLeaveProgram.Controllers
 
             db.LeaveApplications.Add(employeeLeaveApplication);
 
-            return db.SaveChanges(); ;
+            db.SaveChanges();
+            return employeeLeaveApplication;
+        }
+
+        private void SendNewLeaveApplicationEmail(LeaveApplication employeeLeaveApplication)
+        {
+            var url = $"http://{Request.Url.Host}:{Request.UrlReferrer.Port}{Url.Action("Approval", "Manager")}";
+            var body = $@"
+                        <p>Leave application submitted by for your approval</p> 
+                        <p><b>Requested By: </b>{employeeLeaveApplication.Applicant.FirstName} {employeeLeaveApplication.Applicant.LastName}</p>
+                        <p>{url}/{employeeLeaveApplication.Id}
+                        ";
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(employeeLeaveApplication.Manager.Email)); //replace with valid value
+            message.Subject = "Leave Application For Approval";
+            message.Body = body;
+            message.IsBodyHtml = true;
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Send(message);
+
+            }
         }
 
         public ActionResult About()
--- EmployeeLeaveProgram/Controllers/ManagerController.cs
+++ EmployeeLeaveProgram/Controllers/ManagerController.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeLeaveProgram.Data;
 using EmployeeLeaveProgram.Models;
 using System;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace EmployeeLeaveProgram.Controllers
@@ -22,24 +23,62 @@ namespace EmployeeLeaveProgram.Controllers
 
         public ActionResult Approve(string id)
         {
-            db.LeaveApplications
-                .Find(Int64.Parse(id))
-                .Status = LeaveApplicationStatus.Approved;
+            var leaveApplication = db.LeaveApplications
+                .Find(Int64.Parse(id));
+                
+            leaveApplication.Status = LeaveApplicationStatus.Approved;
 
             db.SaveChanges();
+            SendApprovedLeaveApplicationEmail(leaveApplication);
 
             return RedirectToAction("Approval", "Manager");
         }
 
+        private void SendApprovedLeaveApplicationEmail(LeaveApplication employeeLeaveApplication)
+        {
+            var body = $@"
+                        <p>Your leave from {employeeLeaveApplication.StartDate} to {employeeLeaveApplication.EndDate} has been approved</p> 
+                        ";
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(employeeLeaveApplication.Applicant.Email)); 
+            message.Subject = "Leave Application Approved";
+            message.Body = body;
+            message.IsBodyHtml = true;
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Send(message);
+
+            }
+        }
+
         public ActionResult Reject(string id)
         {
-            db.LeaveApplications
-                .Find(Int64.Parse(id))
-                .Status = LeaveApplicationStatus.Rejected;
+            var leaveApplication = db.LeaveApplications
+                  .Find(Int64.Parse(id));
+
+            leaveApplication.Status = LeaveApplicationStatus.Rejected;
 
             db.SaveChanges();
+            SendRejectedLeaveApplicationEmail(leaveApplication);
 
             return RedirectToAction("Approval", "Manager");
+        }
+
+        private void SendRejectedLeaveApplicationEmail(LeaveApplication employeeLeaveApplication)
+        {
+            var body = $@"
+                        <p>Your leave from {employeeLeaveApplication.StartDate} to {employeeLeaveApplication.EndDate} has been rejected</p> 
+                        ";
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(employeeLeaveApplication.Applicant.Email));
+            message.Subject = "Leave Application Rejection";
+            message.Body = body;
+            message.IsBodyHtml = true;
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Send(message);
+
+            }
         }
 
     }
